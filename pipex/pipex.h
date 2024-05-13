11b0f5360cfda5f69018ebd766c06503e3347bc2
Eromon Agbomeirele <eagbomei@c1r5p21.hive.fi>
2024-05-13 17:02:00 +0300

    testing readline

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hzibari <hzibari@student.42.fr>            +#+  +:+       +#+        */
+/*   By: eagbomei <eagbomei@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 13:15:09 by hzibari           #+#    #+#             */
-/*   Updated: 2024/04/08 14:02:59 by hzibari          ###   ########.fr       */
+/*   Updated: 2024/05/13 16:58:57 by eagbomei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "./libft/libft.h"
+# include "../libft/libft.h"
 # include <fcntl.h>
 # include <stdio.h>
 
@@ -32,5 +32,6 @@ void	child_one(int fd1, char *av, t_pipe *struk);
 void	child_two(int fd2, char *av, t_pipe *struk);
 char	*get_command(char **path, char *arg);
 void	put_msg(char *av);
+char	*find_path(char **env);
 
 #endif
