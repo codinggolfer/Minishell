1f41d7e2ba62cdf974cd0fbb96b47e23b96a7d55
Eromon Agbomeirele <eagbomei@c1r4p8.hive.fi>
2024-06-10 16:31:27 +0300

    start for parser

--- parser.c
+++ parser.c
@@ -6,15 +6,25 @@
 /*   By: eagbomei <eagbomei@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:27:07 by halgordziba       #+#    #+#             */
-/*   Updated: 2024/06/10 14:40:15 by eagbomei         ###   ########.fr       */
+/*   Updated: 2024/06/10 16:30:39 by eagbomei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	**cut_cmds(int *find_cmd, char **tokens)
+{
+	
+}
+
 int	parser(t_input *data)
 {
-	t_list	ret;
+	t_list	*ret;
+	t_list	*node;
+	int		find_cmd[2];
+	char	**cmd;
 
+	find_cmd[0] = 0;
+	cmd = cut_cmds(find_cmd, data->tokens);
 	
 }
